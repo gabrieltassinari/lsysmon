0b9a2902b14b739fb985e8cd949aff8315b9aff4
gabrieltassinari <100461539+gabrieltassinari@users.noreply.github.com>
2023-04-21 23:16:02 -0300

    add: sse server/client

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.20
+
+require github.com/rprobaina/lpfs v0.0.0-20230329165809-b2049345150b // indirect
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "net/http"
+    "encoding/json"
+
+    "github.com/rprobaina/lpfs"
+)
+
+type Memory struct{
+	Total int //`json:"Total"`
+	Free int //`json:"Free"`
+}
+
+func sse(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/event-stream")
+    w.Header().Set("Cache-Control", "no-cache")
+    w.Header().Set("Connection", "keep-alive")
+
+    for {
+	mt, _ := lpfs.GetMemTotal()
+	mf, _ := lpfs.GetMemFree()
+
+	msg := Memory{
+		Total: mt, 
+		Free: mf, 
+	}
+
+	b, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Printf("err: %v", err)
+	}
+	
+        fmt.Fprintf(w, "data: %v\n\n", string(b))
+        w.(http.Flusher).Flush()
+
+	time.Sleep(time.Second)
+    }
+}
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	    http.ServeFile(w, r, "index.html")
+    })
+    http.HandleFunc("/sse", sse)
+    http.ListenAndServe(":8080", nil)
+}

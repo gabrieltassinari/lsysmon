d622bd833cf4e7a2bcab73be5e58174bf91d285c
gabrieltassinari <100461539+gabrieltassinari@users.noreply.github.com>
2023-05-27 04:44:31 -0300

    static: Fix sse connection after selection an option

--- static/script.js
+++ static/script.js
@@ -1,13 +1,19 @@
 const response = await fetch("http://localhost:8080/labels");
 const jsonData = await response.json();
 
-const eventSource = new EventSource("/sse");
+let eventSource = new EventSource("/sse");
+let currentPid = 1;
 
-var selectRange= document.getElementById('opts');
+addListeners();
+
+let selectRange = document.getElementById('opts');
 
 selectRange.addEventListener('change', function() {
 	if (this.value != "runtime") {
-		eventSource.close()
+		eventSource.close();
+
+		clearPlotData(ptimePlot);
+		clearPlotData(memoryPlot);
 
 		const jsonData = fetch(`http://localhost:8080/logs?interval=${this.value}`)
 			.then((response) => {
@@ -18,96 +24,103 @@ selectRange.addEventListener('change', function() {
 
 	} else {
 		eventSource = new EventSource("/sse");
+		addListeners();
 	}
 });
 
-eventSource.addEventListener('memory', e => {
-	//console.log(e);
-});
+function addListeners() {
+	eventSource.addEventListener('memory', e => {
+		//console.log(e);
+	});
 
-eventSource.addEventListener('uptime', e => {
-	//console.log(e);
-});
+	eventSource.addEventListener('uptime', e => {
+		//console.log(e);
+	});
 
-eventSource.addEventListener('swap', e => {
-	//console.log(e);
-});
+	eventSource.addEventListener('swap', e => {
+		//console.log(e);
+	});
 
-let currentPid = 1;
+	eventSource.addEventListener('process', e => {
+		let obj = JSON.parse(e.data);
 
-eventSource.addEventListener('process', e => {
-	let obj = JSON.parse(e.data);
+		let rows = document.getElementById("rows")
 
-	let rows = document.getElementById("rows")
+		rows.innerHTML = "";
 
-	rows.innerHTML = "";
+		// Create a table with all processes
+		for (let i = 0; i < obj.length; ++i) {
+			const tr = document.createElement('tr');
 
-	// Create a table with all processes
-	for (let i = 0; i < obj.length; ++i) {
-		const tr = document.createElement('tr');
+			const pid = document.createElement('td');
+			pid.innerText = `${obj[i].Pid}`;
+			tr.append(pid);
 
-		const pid = document.createElement('td');
-		pid.innerText = `${obj[i].Pid}`;
-		tr.append(pid);
+			const comm = document.createElement('td');
+			comm.innerText = `${obj[i].Comm}`;
+			tr.appendChild(comm);
 
-		const comm = document.createElement('td');
-		comm.innerText = `${obj[i].Comm}`;
-		tr.appendChild(comm);
+			const state = document.createElement('td');
+			state.innerText = `${obj[i].State}`;
+			tr.appendChild(state);
 
-		const state = document.createElement('td');
-		state.innerText = `${obj[i].State}`;
-		tr.appendChild(state);
+			tr.onclick = function() {
+				clearPlotData(ptimePlot);
+				clearPlotData(memoryPlot);
 
-		// TODO: Create plots with process stat information
-		tr.onclick = function() {
-			memoryPlot.data.datasets[0].data = [];
-			memoryPlot.data.labels = [];
+				currentPid = i;
+			}
 
-			for (let j = 0; j < 4; ++j)
-				ptimePlot.data.datasets[j].data = [];
-			ptimePlot.data.labels = [];
-
-			currentPid = i;
+			rows.appendChild(tr);
 		}
 
-		rows.appendChild(tr);
-	}
-
-	// TODO: Append data in plots
-	addData(memoryPlot, obj[currentPid].Pid);
-	ptimePlotData(obj[currentPid]);
-});
-
-function ptimePlotData(data) {
-	const labels = ptimePlot.data.labels
-	const array = ptimePlot.data.datasets
-
-	if (array[0].data.length > 50) {
-		for (let i = 0; i < 4; ++i)
-			array[i].data.shift();
-		labels.shift();
-	}
-
-	array[0].data.push(data.Utime);
-	array[1].data.push(data.Stime);
-	array[2].data.push(data.Cutime);
-	array[3].data.push(data.Cstime);
-	labels.push("1");
-
-	ptimePlot.update();
+		addPlotData(memoryPlot, obj[currentPid].Pid);
+		addPlotData(ptimePlot, obj[currentPid]);
+	});
 }
 
-function addData(chart, data) {
-	const labels = chart.data.labels
-	const array = chart.data.datasets[0].data
+function clearPlotData(chart) {
 
-	if (array.length > 50) {
-		labels.shift();
-		array.shift();
+	if (chart == ptimePlot) {
+		for (let j = 0; j < 4; ++j)
+			chart.data.datasets[j].data = [];
+		chart.data.labels = [];
+	} else {
+		chart.data.datasets[0].data = [];
+		chart.data.labels = [];
 	}
 
-	array.push(data);
-	labels.push("1");
+	chart.update();
+}
+
+function addPlotData(chart, data) {
+	const labels = chart.data.labels
+
+	if (chart == ptimePlot) {
+		const array = chart.data.datasets
+
+		if (array[0].data.length > 50) {
+			for (let i = 0; i < 4; ++i)
+				array[i].data.shift();
+			labels.shift();
+		}
+
+		array[0].data.push(data.Utime);
+		array[1].data.push(data.Stime);
+		array[2].data.push(data.Cutime);
+		array[3].data.push(data.Cstime);
+		labels.push("1");
+	} else {
+		const array = chart.data.datasets[0].data
+
+		if (array.length > 50) {
+			array.shift();
+			labels.shift();
+		}
+
+		array.push(data);
+		labels.push("1")
+	}
 
 	chart.update();
 }
